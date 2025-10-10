ed9bdbc4e0e74efe4cd4502d56b0da6923a128e6
magala-richard <rmagala640@gmail.com>
2025-10-09 19:32:03 -0500

    list changes

--- doc/tests/code.py
+++ doc/tests/code.py
@@ -6,6 +6,7 @@ names=  ['nasa_(-9.0, 36.6).met', 'nasa_(-9.3, 37.7).met']
 columns = ["year", "day", "radn", "maxt", "mint", "rain"]
 
 dm = [read_apsim_met(i, skip=6) for i in names]
+
 for df, nam in zip(dm, names):
     saveto = nam.replace('met', 'csv')
     df.columns = columns
