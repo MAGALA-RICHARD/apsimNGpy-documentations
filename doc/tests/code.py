ccd46a4cf3d0f758ff7d8e2bd7224fac6fa7713c
magala-richard <rmagala640@gmail.com>
2025-10-10 09:30:00 -0500

    list changes

--- doc/tests/code.py
+++ doc/tests/code.py
@@ -2,7 +2,7 @@ from apsimNGpy.manager.weathermanager import get_nasa, get_weather, read_apsim_m
 lonlat = [(-9.0, 36.6), (-9.30, 37.7)]
 
 df = [get_weather(i, start = 1984, end = 2024, source='nasa', filename=f"nasa_{i}.met") for i in lonlat ]
-names=  ['nasa_(-9.0, 36.6).met', 'nasa_(-9.3, 37.7).met']
+names =  ['nasa_(-9.0, 36.6).met', 'nasa_(-9.3, 37.7).met']
 columns = ["year", "day", "radn", "maxt", "mint", "rain"]
 
 dm = [read_apsim_met(i, skip=6) for i in names]
